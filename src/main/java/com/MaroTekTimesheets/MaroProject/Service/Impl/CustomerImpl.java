df99bc4c58d0657f84eb2febb722a97e7e215f54
Ahmet Faruk Alkan <84505838+AhmetFarukAlkan@users.noreply.github.com>
2022-08-06 14:56:42 +0300

    Update CustomerImpl.java

--- src/main/java/com/MaroTekTimesheets/MaroProject/Service/Impl/CustomerImpl.java
+++ src/main/java/com/MaroTekTimesheets/MaroProject/Service/Impl/CustomerImpl.java
@@ -13,10 +13,8 @@ import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import java.util.Locale;
+import java.util.*;
+
 
 @Service
 @RequiredArgsConstructor
