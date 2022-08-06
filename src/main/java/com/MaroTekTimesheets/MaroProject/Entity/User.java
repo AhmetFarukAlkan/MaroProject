facd943e42b2ace49e12327d67182a39bc9c3c96
Ahmet Faruk Alkan <84505838+AhmetFarukAlkan@users.noreply.github.com>
2022-08-06 14:49:05 +0300

    Update User.java

--- src/main/java/com/MaroTekTimesheets/MaroProject/Entity/User.java
+++ src/main/java/com/MaroTekTimesheets/MaroProject/Entity/User.java
@@ -12,6 +12,7 @@ import java.awt.*;
 import java.io.IOException;
 import java.security.GeneralSecurityException;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.Date;
 
 @Entity
@@ -21,17 +22,17 @@ import java.util.Date;
 @Getter
 @Setter
 @EqualsAndHashCode(of = {"email"})
-public class User {
-    public User(String idTokenString) throws GeneralSecurityException, IOException {
-        GoogleIdTokenVerifier verifier = new GoogleIdTokenVerifier.Builder(new NetHttpTransport(), new JacksonFactory()).setAudience(Collections.singletonList("574033859076-1htgf8rnd862hdqc8j2a335ne18ganuv.apps.googleusercontent.com"))
-                .build();
-        GoogleIdToken idToken = verifier.verify(idTokenString);
-        IdToken.Payload payload = idToken.getPayload();
-        this.Name = (String) payload.get("given_name");
-        this.email = (String) payload.get("email");
-        this.Status = "A";
-        this.CreateDate = new Date();
-    }
+public class User implements Comparable<User> {
+//     public User(String idTokenString) throws GeneralSecurityException, IOException {
+//         GoogleIdTokenVerifier verifier = new GoogleIdTokenVerifier.Builder(new NetHttpTransport(), new JacksonFactory()).setAudience(Collections.singletonList("574033859076-1htgf8rnd862hdqc8j2a335ne18ganuv.apps.googleusercontent.com"))
+//                 .build();
+//         GoogleIdToken idToken = verifier.verify(idTokenString);
+//         IdToken.Payload payload = idToken.getPayload();
+//         this.Name = (String) payload.get("given_name");
+//         this.email = (String) payload.get("email");
+//         this.Status = "A";
+//         this.CreateDate = new Date();
+//     }
 
     @Id
     @SequenceGenerator(name = "seq_Costumer",allocationSize = 1)
@@ -58,5 +59,9 @@ public class User {
 //    @OneToOne(cascade = CascadeType.ALL)
 //    @JoinColumn(name = "user_role")
     private Role user_role;
-
+    
+    @Override
+    public int compareTo(User o) {
+        return getName().compareTo(o.getName());
+    }
 }
