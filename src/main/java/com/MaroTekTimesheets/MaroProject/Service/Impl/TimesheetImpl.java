fcbc7e853c0c67cb753f9f93c17d04cbf9cca6f6
Ahmet Faruk Alkan <84505838+AhmetFarukAlkan@users.noreply.github.com>
2022-08-06 14:23:07 +0300

    Update TimesheetImpl.java

--- src/main/java/com/MaroTekTimesheets/MaroProject/Service/Impl/TimesheetImpl.java
+++ src/main/java/com/MaroTekTimesheets/MaroProject/Service/Impl/TimesheetImpl.java
@@ -14,10 +14,8 @@ import org.springframework.stereotype.Service;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import java.util.Locale;
+
+import java.util.*;
 
 @Service
 @RequiredArgsConstructor
